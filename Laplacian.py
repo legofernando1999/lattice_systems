3570074bb6f7223c214c9413bf331e423489e286
Fernando Munoz <legofernando1999@gmail.com>
2026-01-30 21:24:26 +0100

    Adds more efficient algorithm for computing eigenvalues.
    Implements a more memory-efficient algorithm specifically for symmetric
    matrices. The general algorithm remains the default for nonsymmetric
    ones.
    
    Updates visualization to include a non-boundary-biased KDE subplot for
    eigenvalues and a rug subplot for singular values of rectangular
    sections.

--- Laplacian.py
+++ Laplacian.py
@@ -34,6 +34,10 @@ def middle_value(k):
         return int(k / 2)
     else:
         return int((k + 1) / 2)
+    
+def is_symmetric(a, tol=1.e-10) -> bool:
+    '''Check if square matrix is symmetric.'''
+    return np.allclose(a, a.T, atol=tol)
 
 def extract_rectangular_section(a, m, n, shift=0):
     '''
@@ -119,7 +123,7 @@ def make_hamiltonian(length, perturb_H=False) -> np.ndarray:
     
     return H
 
-def compute_eigenvalues_and_singular_values(H, rectangular_sections_specs={}) -> tuple[np.ndarray, dict]:
+def compute_eigenvalues_and_singular_values(H, sections_specs={}) -> tuple[np.ndarray, dict]:
     '''
     Compute eigenvalues and singular values of H and rectangular sections extracted from it, respectively.
 
@@ -127,7 +131,7 @@ def compute_eigenvalues_and_singular_values(H, rectangular_sections_specs={}) ->
     ----------
     H : ndarray
         Hamiltonian matrix.
-    rectangular_sections_specs : dict, optional
+    sections_specs : dict, optional
         Specifications for the different rectangular sections to be extracted from H.
 
     Returns
@@ -137,10 +141,23 @@ def compute_eigenvalues_and_singular_values(H, rectangular_sections_specs={}) ->
     H_sections : dict
         Rectangular sections of H together with their singular values.
     '''
-    H_eigenvalues, _ = la.eig(H, check_finite=False)
+    if is_symmetric(H):
+        try:
+            # uses a more efficient algorithm for symmetric matrices
+            H_eigenvalues = la.eigvalsh(H, check_finite=False)
+        except la.LinAlgError:
+            print('Eigenvalue computation did not converge.')
+    else:
+        try:
+            # uses a general algorithm
+            H_eigenvalues = la.eigvals(H, check_finite=False)
+        except la.LinAlgError:
+            print('Eigenvalue computation did not converge.')
+
+        H_eigenvalues = np.float64(H_eigenvalues) # convert to floats
 
     H_sections = {}
-    for k, v in rectangular_sections_specs.items():
+    for k, v in sections_specs.items():
         section = extract_rectangular_section(H, m=v['m'], n=v['n'], shift=v['shift'])
 
         # Singular Value Decomposition: A = U * S * VT (A is any m by n matrix), where the columns of U (m by m) are eigenvectors of A * AT
@@ -152,44 +169,76 @@ def compute_eigenvalues_and_singular_values(H, rectangular_sections_specs={}) ->
 
     return H_eigenvalues, H_sections
 
-def make_histogram(hist_data, fname):
-    '''Make histogram.'''
+def _create_figure(hist_data, fname):
+    '''Create figure and set of subplots.'''
     n_subplots = 1
-    for k in hist_data.keys():
-        if k.startswith('Singular values'):
-            n_subplots += 1
+    height_ratios = None
+    if any(key.startswith('Singular') for key in hist_data):
+        n_subplots = 2
+        height_ratios = [8, 2]
 
     fig, axs = plt.subplots(
         nrows=n_subplots, 
-        sharex=True
+        sharex=True,
+        height_ratios=height_ratios
         )
     
     if type(axs) == matplotlib.axes._axes.Axes:
         axs = np.array([axs], dtype=object)
     
-    palette = sns.color_palette('colorblind', n_colors=n_subplots)
+    palette = sns.color_palette('colorblind', as_cmap=True)
 
-    idx = 0
+    color_idx = 0
     for k, v in hist_data.items():
-        sns.histplot(
-            x=v,
-            ax=axs[idx],
-            binwidth=0.01,
-            color=palette[idx]
+        if k.startswith('Eigenvalues'):
+            # sns.histplot(
+            #     x=v,
+            #     ax=axs[0],
+            #     binwidth=2/10,
+            #     color=palette[0],
+            #     )
+
+            # Mirror data points near boundaries, calculate KDE and then ignore reflected part in order to fix Boundary Bias.
+            left_boundary = 0
+            right_boundary = 2
+            right_reflected_v = 2 * right_boundary - v
+            left_reflected_v = 2 * left_boundary - v
+            reflected_v = np.append(left_reflected_v, np.append(v, right_reflected_v))
+
+            sns.kdeplot(
+                x=reflected_v,
+                ax=axs[0],
+                color=palette[0],
+                fill=True,
+                clip=(0, 2) # Do not evaluate the density outside of these limits.
             )
-        
-        axs[idx].set_title(k)
-        
-        idx += 1
+            
+            axs[0].set_title(k)
+
+        else: # Singular values
+            sns.rugplot(
+                x=v,
+                ax=axs[1],
+                color=palette[color_idx],
+                height=0.75
+            )
+
+            color_idx += 1
+    
+    if n_subplots == 2:
+        axs[1].set_title('Singular values')
+        axs[1].set_yticks([])
+
+    fig.tight_layout()
     
     fig.savefig(
         fname=fname,
         dpi=800
         )
 
-def generate_histogram(length, H_perturbed, H_eigenvalues, H_sections):
+def generate_plot(length, H_perturbed, H_eigenvalues, H_sections):
     '''
-    Prepare plotting data and create histogram.
+    Prepare plotting data and create figure.
 
     Parameters
     ----------
@@ -204,36 +253,34 @@ def generate_histogram(length, H_perturbed, H_eigenvalues, H_sections):
     '''
     if H_perturbed:
         eigenvalues_plot_title = 'Eigenvalues perturbed Hamiltonian'
-        fname = f'{PLOTS_DIR}/hist_plot_perturbed_{length}.png'
+        fname = f'{PLOTS_DIR}/hist_perturbed_L={length}.png'
     else: 
         eigenvalues_plot_title = 'Eigenvalues nonperturbed Hamiltonian'
-        fname = f'{PLOTS_DIR}/hist_plot_nonperturbed_{length}.png'
+        fname = f'{PLOTS_DIR}/hist_nonperturbed_L={length}.png'
 
-    eigenvalues = np.sort(np.float32(H_eigenvalues)) # sort and convert to floats
-
-    hist_data = {eigenvalues_plot_title: eigenvalues}
+    hist_data = {eigenvalues_plot_title: H_eigenvalues}
 
     for k, v in H_sections.items():
         hist_data[f'Singular values {k}'] = v['sv']
     
-    make_histogram(hist_data, fname)
+    _create_figure(hist_data, fname)
 
 def free_hamiltonian():
     '''
     Compute the spectrum of the Hamiltonian of the one-dimensional time-independent free Schroedinger equation with periodic boundary conditions.
     '''
-    L = 100
+    L = 500
     perturb_H = False
 
     H = make_hamiltonian(length=L, perturb_H=perturb_H)
 
-    rectangular_sections_specs = {
-        'first section': dict(m=13, n=11, shift=0),
-        'second section': dict(m=13, n=11, shift=10)
+    sections_specs = {
+        # 'first section': dict(m=13, n=11, shift=0),
+        # 'second section': dict(m=13, n=11, shift=30)
     }
-    H_eigenvalues, H_sections = compute_eigenvalues_and_singular_values(H, rectangular_sections_specs)
+    H_eigenvalues, H_sections = compute_eigenvalues_and_singular_values(H, sections_specs)
 
-    generate_histogram(L, perturb_H, H_eigenvalues, H_sections)
+    generate_plot(L, perturb_H, H_eigenvalues, H_sections)
 
 if __name__ == '__main__':
     free_hamiltonian()
